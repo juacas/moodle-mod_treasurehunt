bfc0d61a80bc78d98e02db39f85546713a0df7d4
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2017-10-06 14:05:29 +0200

    Greek translation added (thanks to Spyros Zoupanos)

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_treasurehunt';
-$plugin->version = 2017080801;
-$plugin->release = 'v1.1.1';
+$plugin->version = 2017100600;
+$plugin->release = 'v1.1.2';
 $plugin->requires = 2015051100; // Moodle 2.9.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array();
