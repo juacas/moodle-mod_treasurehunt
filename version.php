75d7daacc51137869a73da6890437201596c1ebc
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2017-10-10 17:43:53 +0200

    Release maintenance v1.1.4

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_treasurehunt';
-$plugin->version = 2017101001;
-$plugin->release = 'v1.1.3';
+$plugin->version = 2017101002;
+$plugin->release = 'v1.1.4';
 $plugin->requires = 2015051100; // Moodle 2.9.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array();
