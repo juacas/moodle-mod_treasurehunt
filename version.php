50a1315514efdebc1b1836de04e30e5cef899d8a
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2017-11-29 20:44:44 +0100

    v1.2.8 Hotfix for IOS web browser upgrade.
    Broke deprecated target-densitydpi=medium-dpi meta tag.
    Clue text moved to History panel.

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_treasurehunt';
-$plugin->version = 2017110600;
-$plugin->release = 'v1.2.7';
+$plugin->version = 2017112900;
+$plugin->release = 'v1.2.8';
 $plugin->requires = 2015051100; // Moodle 2.9.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array();
