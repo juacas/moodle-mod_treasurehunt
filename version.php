86710c69fab69edea131d3945f94d5ab88e257ff
Juan Pablo <juan.pablo.de.castro@gmail.com>
2020-01-26 18:29:15 +0100

    Release v1.2.22

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_treasurehunt';
-$plugin->version = 2018091200;
-$plugin->release = 'v1.2.21';
+$plugin->version = 2020012600;
+$plugin->release = 'v1.2.22';
 $plugin->requires = 2015051100; // Moodle 2.9.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array();
