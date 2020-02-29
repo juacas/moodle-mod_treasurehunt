2128a85a4971167def051bb3c3c2afa044068863
huorwhisp <huorwhisp@gmail.com>
2020-02-29 10:17:19 +0100

    Avanzada la creación de páginas popup. Integrado geolocalización en el panel de búsquedas

---
+++ amd/src/osm-geocoder.js
@@ -0,0 +1,35 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @module    mod_treasurehunt/play
+ * @package   mod_treasurehunt
+ * @copyright 2016 onwards Adrian Rodriguez Fernandez <huorwhisp@gmail.com>, Juan Pablo de Castro <jpdecastro@tel.uva.es>
+ * @author Adrian Rodriguez <huorwhisp@gmail.com>
+ * @author Juan Pablo de Castro <jpdecastro@tel.uva.es>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+define(["jquery"], function($) {
+  const functions = {
+    search: query => {
+      return $.ajax({
+        method: "GET",
+        url: "https://nominatim.openstreetmap.org/search",
+        data: `format=json&q=${query}`
+      });
+    }
+  };
+  return functions;
+});
--- play.php
+++ play.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Treasurehunt for Moodle
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,16 +16,14 @@
 // along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
- *
- * @package mod_treasurehunt
  * @copyright 2016 onwards Adrian Rodriguez Fernandez <huorwhisp@gmail.com>, Juan Pablo de Castro
  *            <jpdecastro@tel.uva.es>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 // Replace treasurehunt with the name of your module and remove this line.
-require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
-require_once("$CFG->dirroot/mod/treasurehunt/locallib.php");
-require_once($CFG->libdir . '/formslib.php');
+require_once dirname(dirname(dirname(__FILE__))).'/config.php';
+require_once "{$CFG->dirroot}/mod/treasurehunt/locallib.php";
+require_once $CFG->libdir.'/formslib.php';
 
 global $USER;
 
@@ -64,7 +63,7 @@ $output = $PAGE->get_renderer('mod_treasurehunt');
 //$PAGE->requires->jquery();
 // jquerymobile is tied to jquery 2.1.4.
 //$PAGE->requires->jquery();
-$PAGE->requires->js('/mod/treasurehunt/js/jquery2/jquery-2.1.4.min.js');
+// $PAGE->requires->js('/mod/treasurehunt/js/jquery2/jquery-2.1.4.min.js');
 
 // Nicescroll is incompatible with webkit in IOS 11 $PAGE->requires->js('/mod/treasurehunt/js/jquery.nicescroll.min.js');
 // Adds support for QR scan.
@@ -75,14 +74,19 @@ $user->id = $USER->id;
 $user->fullname = fullname($USER);
 $user->pic = $output->user_picture($USER);
 $custommapping = treasurehunt_get_custommappingconfig($treasurehunt, $context);
-$PAGE->requires->js_call_amd('mod_treasurehunt/play', 'playtreasurehunt',
-        array( $cm->id, $cm->instance, intval($treasurehunt->playwithoutmoving),
-                        intval($treasurehunt->groupmode), $lastattempttimestamp, $lastroadtimestamp, $gameupdatetime,
-                        $treasurehunt->tracking, $user, $custommapping));
+$PAGE->requires->js_call_amd(
+    'mod_treasurehunt/play',
+    'playtreasurehunt',
+    array($cm->id, $cm->instance, intval($treasurehunt->playwithoutmoving),
+        intval($treasurehunt->groupmode), $lastattempttimestamp, $lastroadtimestamp, $gameupdatetime,
+        $treasurehunt->tracking, $user, $custommapping, )
+);
 $PAGE->requires->js_call_amd('mod_treasurehunt/tutorial', 'playpage');
 $PAGE->requires->css('/mod/treasurehunt/css/introjs.css');
-$PAGE->requires->css('/mod/treasurehunt/css/jquerymobile.css');
-$PAGE->requires->css('/mod/treasurehunt/css/treasure.css');
+// $PAGE->requires->css('/mod/treasurehunt/css/jquerymobile.css');
+$PAGE->requires->css('/mod/treasurehunt/css/play.css');
+
+$PAGE->requires->css('/mod/treasurehunt/css/ol.css');
 
 $PAGE->set_pagelayout('embedded');
 
@@ -98,50 +102,50 @@ echo $output->header();
 // Polyfill service adds compatibility to old browsers like IOS WebKit for requestAnimationFrame.
 echo '<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch,requestAnimationFrame,Element.prototype.classList,URL"></script>';
 
-echo treasurehunt_view_play_page($treasurehunt, $cm->id, $user);
+echo treasurehunt_view_play_page($treasurehunt, $cm->id);
 
 // Log event.
-require_once('classes/event/player_entered.php');
+require_once 'classes/event/player_entered.php';
 $event = \mod_treasurehunt\event\player_entered::create(array(
-                'objectid' => $id,
-                'context' => $context,
+    'objectid' => $id,
+    'context' => $context,
 ));
-$event->add_record_snapshot("treasurehunt", $treasurehunt);
+$event->add_record_snapshot('treasurehunt', $treasurehunt);
 $event->trigger();
 
 // Finish the page.
 $page = $output->footer();
 
 // JPC: Generate a global variable with strings. Moodle 3.8 broke compatibility of core/str with jquery 2.1.4.
-$terms = ["stageovercome", "failedlocation", "stage", "stagename",
-                             "stageclue", "question", "noanswerselected", "timeexceeded",
-                             "searching", "continue", "noattempts", "aerialview", "roadview", 
-                             "noresults", "startfromhere", "nomarks", "updates", "activitytoendwarning",
-                             "huntcompleted", "discoveredlocation", "answerwarning", "error"];
-$strings = [];
-foreach($terms as $term) {
+$terms = array('stageovercome', 'failedlocation', 'stage', 'stagename',
+    'stageclue', 'question', 'noanswerselected', 'timeexceeded',
+    'searching', 'continue', 'noattempts', 'aerialview', 'roadview',
+    'noresults', 'startfromhere', 'nomarks', 'updates', 'activitytoendwarning',
+    'huntcompleted', 'discoveredlocation', 'answerwarning', 'error', );
+$strings = array();
+foreach ($terms as $term) {
     $strings[$term] = get_string($term, 'treasurehunt');
 }
 $i18n = json_encode($strings);
 echo <<<I18N
 <!-- Internationalization strings for the player -->
 <script type="text/javascript">
-i18nplay = $i18n;
+i18nplay = {$i18n};
 </script>
 
 I18N;
 
 // Patch: disable modules that are jquery 2.1.4 uncompatible/unnecesary
-$disable = [
+$disable = array(
     'core/notification',
     'block_navigation/navblock',
     'block_settings/settingsblock',
     // 'theme_boost/loader',
     'core/log',
     'core/page_global',
-];
-$pagefiltered= $page;
+);
+$pagefiltered = $page;
 foreach ($disable as $module) {
-    $pagefiltered = str_replace("M.util.js_pending('$module')", "//M.util.js_pending('$module')", $pagefiltered);
+    $pagefiltered = str_replace("M.util.js_pending('{$module}')", "//M.util.js_pending('{$module}')", $pagefiltered);
 }
 echo $pagefiltered;
