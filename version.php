98478e5a2bb9b9f7194dd38f0de437aeacd96249
Juan Pablo <juan.pablo.de.castro@gmail.com>
2020-03-23 13:33:44 +0100

    Fix for #43

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_treasurehunt';
-$plugin->version = 2020032300;
-$plugin->release = 'v1.3.1b-hotfix';
+$plugin->version = 2020032301;
+$plugin->release = 'v1.3.1c-hotfix';
 $plugin->requires = 2015051100; // Moodle 2.9.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array();
